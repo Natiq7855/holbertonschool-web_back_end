833a85188be82f12efe63c467acedc37435c7df4
Natiq7855 <natabbasli@gmail.com>
2026-05-24 01:23:12 +0400

    SDF

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,10 +2,10 @@
 """
 This module provides a type-annotated function for string concatenation.
 """
-
+import math
 
 def concat(n: float) -> float:
     """
     Takes two strings and returns them concatenated together.
     """
-    return n
+    return math.floor(n)
