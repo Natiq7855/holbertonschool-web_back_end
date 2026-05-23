600377303d9142115aac3fb4ff05588e87579ad1
Natiq7855 <natabbasli@gmail.com>
2026-05-24 01:32:46 +0400

    SDF

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module provides a type-annotated function to invert float number to str."""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
