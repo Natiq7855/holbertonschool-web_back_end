2a7442c8ebecaa8f34dead7e9d49147a5f3f0b52
Natiq7855 <natabbasli@gmail.com>
2026-05-24 01:51:41 +0400

    SDF

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,6 +5,7 @@ of elements within an iterable.
 """
 from typing import Iterable, List, Tuple, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Takes an iterable of sequences and returns a list of tuples containing
