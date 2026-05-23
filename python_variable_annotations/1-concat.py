39c69727c36186bd461c0868fbf8a33ca3cb0bf0
Natiq7855 <natabbasli@gmail.com>
2026-05-24 01:20:20 +0400

    SDF

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+"""
+This module provides a type-annotated function for string concatenation.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes two strings and returns them concatenated together.
+    """
+    return str1 + str2
