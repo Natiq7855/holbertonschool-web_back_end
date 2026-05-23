c9eda50e26d02d7f5e8cb76647909b8ed41ffa74
Natiq7855 <natabbasli@gmail.com>
2026-05-24 01:43:31 +0400

    SDF

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ This module provides a type-annotated function to sum a list of floats.
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Takes a list of floats as input and returns their sum as a float.
     """
