5eb1b61270b11ecd40e947ffdae8e1f2c5c8a972
Natiq7855 <natabbasli@gmail.com>
2026-05-25 11:47:17 +0400

    Fix: convert submodule to regular folder

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+import asyncio
+import random
+"""sdfj askganfg kfg"""
+
+async def wait_random(max_delay: int = 10) -> float:
+    """sdkjdfgjdfl fgjdlfndfalnlj asdff"""
+
+    delay = random.uniform(0, max_delay)
+
+    await asyncio.delay(delay)
+    return delay
