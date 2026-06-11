3ffdcf6ae6909bf6efdcd9d421cd1d13efc0ab34
Natiq7855 <natabbasli@gmail.com>
2026-06-11 10:45:14 +0400

    SDF

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,4 +7,4 @@ def index_range(page, page_size) -> tuple[int, int]:
 
     start_page = (page - 1) * page_size
     end_page = start_page + page_size
-    return(start_page, end_page)
+    return (start_page, end_page)
