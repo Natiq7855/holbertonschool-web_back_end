ca4bdc47cad9b7f8e437c1474a6b07e8e5d18c19
Natiq7855 <natabbasli@gmail.com>
2026-06-11 10:56:55 +0400

    SDF

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
+"""
+Main pagination module
+"""
 import csv
 import math
 from typing import List
-"""sdfsfdh sdjfsnd dsjf sd"""
 
 
-def index_range(page, page_size) -> tuple[int, int]:
-    """sfjj dfgdf g dfg df dfg"""
-
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """Calculate start and end indexes for a given page and page size."""
     start_page = (page - 1) * page_size
     end_page = start_page + page_size
     return (start_page, end_page)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -33,15 +32,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
-
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Get the requested page from the dataset."""
+        """Get the requested page from the dataset."""
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
+
         data = self.dataset()
         start, end = index_range(page, page_size)
+
         if start >= len(data):
             return []
+
         return data[start:end]
