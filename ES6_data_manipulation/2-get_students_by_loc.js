70cbd6414e8daf62a38b2e71555a0e81b3071fa8
Natiq7855 <natabasli@gmail.com>
2026-07-11 14:14:58 +0400

    SDF

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-export deafault function getStudentsByLocation(students, city) {
+export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) {
     return [];
   }
